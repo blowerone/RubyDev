3bd015a38f12b018998520110722248ad1b2b7e3
Jack <737477675@qq.com>
2016-10-29 22:36:03 +0800

    The learn about Ruby control and loop

---
+++ source/day04/ruby_control.rb
@@ -0,0 +1,116 @@
+﻿#Ruby条件控制
+=begin
+Ruby if...else 语句
+
+语法
+	if conditional [then]
+		code...
+	[elsif conditional [then]
+		code...]...
+	[else
+		code...]
+	end
+通常我们省略保留字 then 。若想在一行内写出完整的 if 式，则必须以 then 隔开条件式和程式区块。如下所示:
+if a == 4 then a = 7 end
+=end
+
+x=1
+if x>2
+	puts "x大于2"
+elsif x<2 and x!=0
+	puts "x 是 1"
+else
+	puts "无法得知 X 的值"
+end
+
+#if...else语句是针对条件为真时执行if内的code语句，条件为假时执行else内的code语句
+#相反的有对应的Ruby unless语句，unless式和 if式作用相反，即如果 conditional 为假，则执行 code。如果 conditional 为真，则执行 else 子句中指定的 code。
+=begin
+	unless conditional [then]
+		code
+	[else
+		code ]
+	end
+=end
+
+x=1
+unless x>2
+   puts "x 小于 2"
+ else
+  puts "x 大于 2"
+end
+
+#Ruby if 修饰符 if修饰词组表示当 if 右边之条件成立时才执行 if 左边的式子。即如果 conditional 为真，则执行 code。
+#code if condition
+$debug=1
+print "debug\n" if $debug
+
+#ruby unless 修饰符 unless式和 if式作用相反，即如果 conditional 为假，则执行 code。如果 conditional 为真，则执行 else 子句中指定的 code
+#code unless conditional
+$var =  1
+print "1 -- 这一行输出\n" if $var
+print "2 -- 这一行不输出\n" unless $var
+
+$var = false
+print "3 -- 这一行输出\n" unless $var
+
+=begin
+Ruby case 语句
+语法
+case expression
+[when expression [, expression ...] [then]
+   code ]...
+[else
+   code ]
+end
+通常我们省略保留字 then 。若想在一行内写出完整的 when 式，则必须以 then 隔开条件式和程式区块。如下所示:
+when a == 4 then a = 7 end
+
+理解这就相当与c语言的 switch...case...default 语句。这里case相当于C语言的switch，when相当于C语言的case，else相当于C语言的default
+因此：
+case expr0
+when expr1, expr2
+   stmt1
+when expr3, expr4
+   stmt2
+else
+   stmt3
+end
+基本上类似于：
+_tmp = expr0
+if expr1 === _tmp || expr2 === _tmp
+   stmt1
+elsif expr3 === _tmp || expr4 === _tmp
+   stmt2
+else
+   stmt3
+end
+=end
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+$age =  5
+case $age
+when 0 .. 2
+    puts "婴儿"
+when 3 .. 6
+    puts "小孩"
+when 7 .. 12
+    puts "child"
+when 13 .. 18
+    puts "少年"
+else
+    puts "其他年龄段的"
+end
+
+#当case的"表达式"部分被省略时，将计算第一个when条件部分为真的表达式。
+foo = false
+bar = true
+quu = false
+
+case
+when foo then puts 'foo is true'
+when bar then puts 'bar is true'
+when quu then puts 'quu is true'
+end
+# 显示 "bar is true"
---
+++ source/day04/ruby_loop.rb
@@ -0,0 +1,272 @@
+﻿#Ruby 循环
+=begin
+① while 语句
+
+② while 修饰符
+
+③ until 语句
+
+④ until 修饰符
+
+⑤ for 语句
+
+⑥ break 语句
+
+⑦ next语句
+
+⑧ redo 语句
+
+⑨ retry 语句
+
+=end
+
+=begin
+Ruby while 语句
+语法
+while conditional [do]
+   code
+end
+或者
+<pre>
+while conditional [:]
+   code
+end
+当 conditional 为真时，执行 code。
+语法中 do 或 : 可以省略不写。但若要在一行内写出 while 式，则必须以 do 或 : 隔开条件式或程式区块。
+=end
+$i = 0
+$num = 5
+
+while $i < $num  do
+   puts("在循环语句中 i = #$i" )
+   $i +=1
+end
+
+=begin
+Ruby while 修饰符
+语法
+code while condition
+
+或者
+
+begin 
+  code 
+end while conditional
+当 conditional 为真时，执行 code。
+如果 while 修饰符跟在一个没有 rescue 或 ensure 子句的 begin 语句后面，code 会在 conditional 判断之前执行一次。
+
+理解相当于C语言中的do...when语句，判断条件之前会先执行code代码
+=end
+
+$i = 0
+$num = 5
+begin
+   puts("在循环语句中 i = #$i" )
+   $i +=1
+end while $i < $num
+
+#在循环语句中 i = 0
+#在循环语句中 i = 1
+#在循环语句中 i = 2
+#在循环语句中 i = 3
+#在循环语句中 i = 4
+
+=begin
+Ruby until 语句
+until conditional [do]
+   code
+end
+当 conditional 为假时，执行 code。
+语法中 do 可以省略不写。但若要在一行内写出 until 式，则必须以 do 隔开条件式或程式区块。
+=end
+
+$i = 0
+$num = 5
+
+until $i > $num  do
+   puts("在循环语句中 i = #$i" )
+   $i +=1;
+end
+#在循环语句中 i = 0
+#在循环语句中 i = 1
+#在循环语句中 i = 2
+#在循环语句中 i = 3
+#在循环语句中 i = 4
+#在循环语句中 i = 5
+
+=begin
+Ruby until 修饰符
+语法
+code until conditional
+
+或者
+
+begin
+   code
+end until conditional
+当 conditional 为 false 时，执行 code。
+如果 until 修饰符跟在一个没有 rescue 或 ensure 子句的 begin 语句后面，code 会在 conditional 判断之前执行一次。
+=end
+
+$i = 0
+$num = 5
+begin
+   puts("在循环语句中 i = #$i" )
+   $i +=1;
+end until $i > $num
+
+=begin
+Ruby for 语句
+语法
+for variable [, variable ...] in expression [do]
+   code
+end
+先计算表达式得到一个对象，然后针对 expression 中的每个元素分别执行一次 code。
+=end
+
+for i in 0..5
+   puts "局部变量的值为 #{i}"
+end
+
+#for...in 循环几乎是完全等价于：
+#(expression).each do |variable[, variable...]| code end
+#但是，for 循环不会为局部变量创建一个新的作用域。
+#语法中 do 可以省略不写。但若要在一行内写出 for 式，则必须以 do 隔开条件式或程式区块。
+
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+(0..5).each do |i|
+   puts "局部变量的值为 #{i}"
+end
+
+=begin
+#break 语句
+Ruby break 语句
+语法
+break
+终止最内部的循环。如果在块内调用，则终止相关块的方法（方法返回 nil）。
+=end
+
+for i in 0..5
+   if i > 2 then
+      break
+   end
+   puts "局部变量的值为 #{i}"
+end
+
+=begin
+Ruby next 语句
+语法
+next
+跳到循环的下一个迭代。如果在块内调用，则终止块的执行（yield 表达式返回 nil）。
+
+理解相当于C语言中的continue
+
+=end
+
+for i in 0..5
+   if i < 2 then
+      next
+   end
+   puts "局部变量的值为 #{i}"
+end
+
+=begin
+Ruby redo 语句
+语法
+redo
+重新开始最内部循环的该次迭代，不检查循环条件。如果在块内调用，则重新开始 yield 或 call。
+=end
+
+for i in 0..5
+   if i < 2 then
+      puts "局部变量的值为 #{i}"
+      redo
+   end
+end
+#这将产生以下结果，并会进入一个无限循环：
+#局部变量的值为 0
+#局部变量的值为 0
+#............................
+
+=begin
+Ruby retry 语句
+注意：1.9以及之后的版本不支持在循环中使用retry。
+语法
+retry
+如果 retry 出现在 begin 表达式的 rescue 子句中，则从 begin 主体的开头重新开始。
+
+begin
+   do_something   # 抛出的异常
+rescue
+   # 处理错误
+   retry          # 重新从 begin 开始
+end
+如果 retry 出现在迭代内、块内或者 for 表达式的主体内，则重新开始迭代调用。迭代的参数会重新评估。
+for i in 1..5
+   retry if some_condition # 重新从 i == 1 开始
+=end
+
+for i in 1..5
+   retry if  i > 2
+   puts "局部变量的值为 #{i}"
+end
+
+#这将产生以下结果，并会进入一个无限循环：
+#局部变量的值为 1
+#局部变量的值为 2
+#局部变量的值为 1
+#局部变量的值为 2
+#局部变量的值为 1
+#局部变量的值为 2
+#............................
+
+#理解redo是不断的执行一个真条件下的语句，
+
+#retry达到条件为真时，retry是不断的执行条件为假时的循环语句
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
