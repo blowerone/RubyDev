83dbbea586a2b71137021da3b4586c11c193a231
Jack <737477675@qq.com>
2016-11-04 22:29:14 +0800

    The learn about ruby datatype

---
+++ source/day06/ruby_dataType.rb
@@ -0,0 +1,384 @@
+﻿=begin
+>>Ruby 字符串（String）
+
+Ruby 中的 String 对象用于存储或操作一个或多个字节的序列。
+Ruby 字符串分为单引号字符串（'）和双引号字符串（"），区别在于双引号字符串能够支持更多的转义字符。
+
+=end
+
+#>>单引号字符串
+'这是一个 Ruby 程序的字符串'
+
+'Won\'t you read O\'Reilly\'s book?' #如果您需要在单引号字符串内使用单引号字符，那么需要在单引号字符串使用反斜杠(\)，这样 Ruby 解释器就不会认为这个单引号字符是字符串的终止符号：
+
+#>>双引号字符串
+#在双引号字符串中我们可以使用 #{} 井号和大括号来计算表达式的值：
+#字符串中嵌入变量：
+
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+name1 = "Joe"
+name2 = "Mary"
+puts "你好 #{name1},  #{name2} 在哪?"
+
+#字符串中进行数学运算：
+
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+x, y, z = 12, 36, 72
+puts "x 的值为 #{ x }"
+puts "x + y 的值为 #{ x + y }"
+puts "x + y + z 的平均值为 #{ (x + y + z)/3 }"
+
+=begin
+Ruby 中还支持一种采用 %q 和 %Q 来引导的字符串变量，%q 使用的是单引号引用规则，
+而 %Q 是双引号引用规则，后面再接一个 (! [ { 等等的开始界定符和与 } ] ) 等等的末尾界定符。
+跟在q或Q后面的字符是分界符.分界符可以是任意一个非字母数字的单字节字符.如:[,{,(,<,!等,
+字符串会一直读取到发现相匹配的结束符为止.
+=end
+
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+desc1 = %Q{Ruby 的字符串可以使用 '' 和 ""。}
+desc2 = %q|Ruby 的字符串可以使用 '' 和 ""。|
+
+puts desc1
+puts desc2
+
+=begin
+字符串内建方法
+我们需要有一个 String 对象的实例来调用 String 方法。下面是创建 String 对象实例的方式：
+new [String.new(str="")]
+这将返回一个包含 str 副本的新的字符串对象。现在，使用 str 对象，我们可以调用任意可用的实例方法。例如：
+=end
+
+#!/usr/bin/ruby
+
+myStr = String.new("THIS IS TEST")
+foo = myStr.downcase
+
+puts "#{foo}"
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+=begin
+Ruby 数组（Array）
+Ruby 数组是任何对象的有序整数索引集合。数组中的每个元素都与一个索引相关，并可通过索引进行获取。
+数组的索引从 0 开始，这与 C 或 Java 中一样。一个负数的索相对于数组的末尾计数的，也就是说，索引为 -1 表示数组的最后一个元素，-2 表示数组中的倒数第二个元素，依此类推。
+Ruby 数组可存储诸如 String、 Integer、 Fixnum、 Hash、 Symbol 等对象，甚至可以是其他 Array 对象。
+Ruby 数组不需要指定大小，当向数组添加元素时，Ruby 数组会自动增长。
+=end
+
+#>>创建数组
+#有多种方式创建或初始化数组。一种方式是通过 new 类方法：
+#	names = Array.new
+#您可以在创建数组的同时设置数组的大小：
+#	names = Array.new(20)
+#数组 names 的大小或长度为 20 个元素。您可以使用 size 或 length 方法返回数组的大小：
+
+#!/usr/bin/ruby
+
+names = Array.new(20)
+puts names.size  # 返回 20
+puts names.length # 返回 20
+
+#您可以给数组中的每个元素赋值，如下所示：
+#!/usr/bin/ruby
+
+names = Array.new(4, "mac")
+
+puts "#{names}"
+
+#您也可以使用带有 new 的块，每个元素使用块中的计算结果来填充：
+
+#!/usr/bin/ruby
+
+nums = Array.new(10) { |e| e = e * 2 }
+
+puts "#{nums}"
+
+#数组还有另一种方法，[]，如下所示：
+nums = Array.[](1, 2, 3, 4,5)
+
+#数组创建的另一种形式如下所示：
+nums = Array[1, 2, 3, 4,5]
+
+#>>数组内建方法
+#我们需要有一个 Array 对象的实例来调用 Array 方法。下面是创建 Array 对象实例的方式：
+#Array.[](...) [or] Array[...] [or] [...]
+#这将返回一个使用给定对象进行填充的新数组。现在，使用创建的对象，我们可以调用任意可用的方法。例如：
+
+#!/usr/bin/ruby
+
+digits = Array(0..9)
+
+num = digits.at(6)
+
+puts "#{num}"
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+=begin
+Ruby 哈希（Hash）
+
+哈希（Hash）是类似 "key" => "value" 这样的键值对集合。哈希类似于一个数组，只不过它的索引不局限于使用数字。
+Hash 的索引（或者叫"键"）几乎可以是任何对象。
+Hash 虽然和数组类似，但却有一个很重要的区别：Hash 的元素没有特定的顺序。 如果顺序很重要的话就要使用数组了。
+=end
+
+#创建哈希
+#①与数组一样，有各种不同的方式来创建哈希。您可以通过 new 类方法创建一个空的哈希：
+
+months = Hash.new
+puts months     #输出{}
+
+#②您也可以使用 new 创建带有默认值的哈希，不带默认值的哈希是 nil：
+
+months = Hash.new( "month" )
+puts "#{months[0]}"
+
+#或
+
+months = Hash.new "month"
+puts "#{months[0]}"
+
+#!/usr/bin/ruby
+
+H = Hash["a" => 100, "b" => 200]
+
+puts "#{H['a']}"
+puts "#{H['b']}"
+
+=begin
+哈希内置方法
+如果需要调用 Hash 方法，需要先实例化一个 Hash 对象。下面是创建 Hash 对象实例的方式：
+Hash[[key =>|, value]* ] or
+
+Hash.new [or] Hash.new(obj) [or]
+
+Hash.new { |hash, key| block }
+=end
+#这将返回一个使用给定对象进行填充的新的哈希。现在，使用创建的对象，我们可以调用任意可用的方法。例如：
+
+#!/usr/bin/ruby
+
+$, = ", "
+months = Hash.new( "month" )
+
+months = {"1" => "January", "2" => "February"}
+
+keys = months.keys
+
+puts "#{keys}"
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+=begin
+>>Ruby 日期 & 时间（Date & Time）
+
+Time 类在 Ruby 中用于表示日期和时间。它是基于操作系统提供的系统日期和时间之上。
+该类可能无法表示 1970 年之前或者 2038 年之后的日期。
+本教程将让您熟悉日期和时间的所有重要的概念。
+=end
+
+#创建当前的日期和时间
+#下面是获取当前的日期和时间的简单实例：
+
+#!/usr/bin/ruby -w
+# -*- coding: UTF-8 -*-
+
+time1 = Time.new
+
+puts "当前时间 : " + time1.inspect
+
+# Time.now 功能相同
+time2 = Time.now
+puts "当前时间 : " + time2.inspect
+
+#获取 Date & Time 组件
+#我们可以使用 Time 对象来获取各种日期和时间的组件。请看下面的实例：
+
+
+#!/usr/bin/ruby -w
+# -*- coding: UTF-8 -*-
+
+time = Time.new
+
+# Time 的组件
+#!/usr/bin/ruby -w
+# -*- coding: UTF-8 -*-
+
+time = Time.new
+
+# Time 的组件
+puts "当前时间 : " + time.inspect
+puts time.year    # => 日期的年份
+puts time.month   # => 日期的月份（1 到 12）
+puts time.day     # => 一个月中的第几天（1 到 31）
+puts time.wday    # => 一周中的星期几（0 是星期日）
+puts time.yday    # => 365：一年中的第几天
+puts time.hour    # => 23：24 小时制
+puts time.min     # => 59
+puts time.sec     # => 59
+puts time.usec    # => 999999：微秒
+puts time.zone    # => "UTC"：时区名称
+
+#Time.utc、Time.gm 和 Time.local 函数
+#这些函数可用于格式化标准格式的日期，如下所示：
+
+# July 8, 2008
+Time.local(2008, 7, 8)  
+# July 8, 2008, 09:10am，本地时间
+Time.local(2008, 7, 8, 9, 10)   
+# July 8, 2008, 09:10 UTC
+Time.utc(2008, 7, 8, 9, 10)  
+# July 8, 2008, 09:10:11 GMT （与 UTC 相同）
+Time.gm(2008, 7, 8, 9, 10, 11)  
+
+#!/usr/bin/ruby -w
+
+time = Time.new
+
+values = time.to_a
+p values
+
+#时区和夏令时
+#您可以使用 Time 对象来获取与时区和夏令时有关的所有信息，如下所示：
+
+
+time = Time.new
+
+# 这里是解释
+time.zone       # => "UTC"：返回时区
+time.utc_offset # => 0：UTC 是相对于 UTC 的 0 秒偏移
+time.zone       # => "PST"（或其他时区）
+time.isdst      # => false：如果 UTC 没有 DST（夏令时）
+time.utc?       # => true：如果在 UTC 时区
+time.localtime  # 转换为本地时区
+time.gmtime     # 转换回 UTC
+time.getlocal   # 返回本地区中的一个新的 Time 对象
+time.getutc     # 返回 UTC 中的一个新的 Time 对象
+
+#格式化时间和日期
+#有多种方式格式化日期和时间。下面的实例演示了其中一部分：
+
+
+#!/usr/bin/ruby -w
+time = Time.new
+
+puts time.to_s
+puts time.ctime
+puts time.localtime
+puts time.strftime("%Y-%m-%d %H:%M:%S")
+
+#时间算法
+#您可以用时间做一些简单的算术，如下所示：
+now = Time.now           # 当前时间
+puts now
+
+past = now - 10          # 10 秒之前。Time - number => Time
+puts past
+
+future = now + 10        # 从现在开始 10 秒之后。Time + number => Time
+puts future
+
+diff = future - now      # => 10  Time - Time => 秒数
+puts diff
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+=begin
+>>Ruby 范围（Range）
+	范围（Range）无处不在：a 到 z、 0 到 9、等等。Ruby 支持范围，并允许我们以不同的方式使用范围：
+		作为序列的范围
+		作为条件的范围
+		作为间隔的范围
+=end
+
+#作为序列的范围
+#范围的第一个也是最常见的用途是表达序列。序列有一个起点、一个终点和一个在序列产生连续值的方式。
+#Ruby 使用 ''..'' 和 ''...'' 范围运算符创建这些序列。两点形式创建一个包含指定的最高值的范围，三点形式创建一个不包含指定的最高值的范围。
+puts (1..5)        #==> 1, 2, 3, 4, 5
+puts (1...5)       #==> 1, 2, 3, 4
+puts ('a'..'d')    #==> 'a', 'b', 'c', 'd'
+
+#序列 1..100 是一个 Range 对象，包含了两个 Fixnum 对象的引用。如果需要，您可以使用 to_a 方法把范围转换为列表。尝试下面的实例：
+#!/usr/bin/ruby
+
+range1 = (1..10).to_a
+range2 = ('bar'..'bat').to_a
+
+puts "#{range1}"
+puts "#{range2}"
+
+#范围实现了让您可以遍历它们的方法，您可以通过多种方式检查它们的内容：
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+# 指定范围
+digits = 0..9
+
+puts digits.include?(5)
+ret = digits.min
+puts "最小值为 #{ret}"
+
+ret = digits.max
+puts "最大值为 #{ret}"
+
+ret = digits.reject {|i| i < 5 }
+puts "不符合条件的有 #{ret}"
+
+digits.each do |digit|
+   puts "在循环中 #{digit}"
+end
+
+#>>作为条件的范围
+#范围也可以用作条件表达式。例如，下面的代码片段从标准输入打印行，其中每个集合的第一行包含单词 start，最后一行包含单词 end.：
+#while gets
+#   print if /start/../end/
+#end
+
+#范围可以用在 case 语句中：
+
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+score = 70
+
+result = case score
+when 0..40
+	"糟糕的分数"
+when 41..60
+	"快要及格"
+when 61..70
+	"及格分数"
+when 71..100
+   "良好分数"
+else
+	"错误的分数"
+end
+
+puts result
+
+#作为间隔的范围
+#范围的最后一个用途是间隔检测：检查指定值是否在指定的范围内。需要使用 === 相等运算符来完成计算。
+
+#!/usr/bin/ruby
+# -*- coding: UTF-8 -*-
+
+if ((1..10) === 5)
+  puts "5 在 (1..10)"
+end
+
+if (('a'..'j') === 'c')
+  puts "c 在 ('a'..'j')"
+end
+
+if (('a'..'j') === 'z')
+  puts "z 在 ('a'..'j')"
+end
---
+++ source/day06/ruby_fileoperator.rb
@@ -0,0 +1,157 @@
+﻿=begin
+>>Ruby 文件的输入与输出
+
+Ruby 提供了一整套 I/O 相关的方法，在内核（Kernel）模块中实现。所有的 I/O 方法派生自 IO 类。
+类 IO 提供了所有基础的方法，比如 read、 write、 gets、 puts、 readline、 getc 和 printf。
+本章节将讲解所有 Ruby 中可用的基础的 I/O 函数。如需了解更多的函数，请查看 Ruby 的 IO 类。
+=end
+
+#puts 语句
+#在前面的章节中，您赋值给变量，然后使用 puts 语句打印输出。
+
+#gets 语句
+#gets 语句可用于获取来自名为 STDIN 的标准屏幕的用户输入。
+
+#!/usr/bin/ruby
+
+puts "Enter a value :"
+val = gets
+puts val
+
+#putc 语句
+#与 puts 语句不同，puts 语句输出整个字符串到屏幕上，而 putc 语句可用于依次输出一个字符。
+
+#!/usr/bin/ruby
+
+str="Hello Ruby!"
+putc str
+
+#print 语句
+#print 语句与 puts 语句类似。唯一的不同在于 puts 语句在输出内容后会跳到下一行，而使用 print 语句时，光标定位在同一行。
+
+#!/usr/bin/ruby
+
+print "Hello World"
+print "Good Morning\n"
+
+=begin
+>>打开和关闭文件
+
+截至现在，您已经读取并写入标准输入和输出。现在，我们将看看如何操作实际的数据文件。
+
+File.new 方法
+	您可以使用 File.new 方法创建一个 File 对象用于读取、写入或者读写，读写权限取决于 mode 参数。最后，您可以使用 File.close 方法来关闭该文件。
+	语法
+	aFile = File.new("filename", "mode")
+		# ... 处理文件
+	aFile.close
+	
+File.open 方法
+	您可以使用 File.open 方法创建一个新的 file 对象，并把该 file 对象赋值给文件。但是，File.open 和 File.new 方法之间有一点不同。不同点是 File.open 方法可与块关联，而 File.new 方法不能。
+	File.open("filename", "mode") do |aFile|
+		# ... process the file
+	end
+=end
+
+
+#>>读取和写入文件
+#用于简单 I/O 的方法也可用于所有 file 对象。所以，gets 从标准输入读取一行，aFile.gets 从文件对象 aFile 读取一行。
+#但是，I/O 对象提供了访问方法的附加设置，为我们提供了便利。
+
+#sysread 方法
+
+#!/usr/bin/ruby
+
+aFile = File.new("input.txt", "r")
+if aFile
+   content = aFile.sysread(20) #您可以使用方法 sysread 来读取文件的内容。当使用方法 sysread 时，您可以使用任意一种模式打开文件。
+   puts content
+else
+   puts "Unable to open file!"
+end
+
+#syswrite 方法
+
+#!/usr/bin/ruby
+
+aFile = File.new("input.txt", "r+")
+if aFile
+   aFile.syswrite("ABCDEF")  #您可以使用方法 syswrite 来向文件写入内容。当使用方法 syswrite 时，您需要以写入模式打开文件
+else
+   puts "Unable to open file!"
+end
+
+#each_byte 方法
+
+#!/usr/bin/ruby
+
+aFile = File.new("input.txt", "r+")
+if aFile
+   aFile.syswrite("ABCDEF")
+   aFile.rewind
+   aFile.each_byte {|ch| putc ch; putc ?. } #该方法属于类 File。方法 each_byte 是个可以迭代字符串中每个字符
+else
+   puts "Unable to open file!"
+end
+
+#IO.readlines 方法
+#类 File 是类 IO 的一个子类。类 IO 也有一些用于操作文件的方法。
+#IO.readlines 是 IO 类中的一个方法。该方法逐行返回文件的内容。下面的代码显示了方法 IO.readlines 的使用：
+
+#!/usr/bin/ruby
+
+arr = IO.readlines("input.txt")
+puts arr[0]
+puts arr[1]
+
+#IO.foreach 方法
+#该方法也逐行返回输出。方法 foreach 与方法 readlines 之间不同的是，方法 foreach 与块相关联。
+#但是，不像方法 readlines，方法 foreach 不是返回一个数组。例如：
+
+#!/usr/bin/ruby
+
+IO.foreach("input.txt"){|block| puts block} #这段代码将把文件 test 的内容逐行传给变量 block，然后输出将显示在屏幕上。
+
+#重命名和删除文件
+#您可以通过 rename 和 delete 方法重命名和删除文件。
+#下面的实例重命名一个已存在文件 test1.txt：
+
+#!/usr/bin/ruby
+
+# 重命名文件 test1.txt 为 test2.txt
+File.rename( "test1.txt", "test2.txt" )
+#下面的实例删除一个已存在文件 test2.txt：
+
+#!/usr/bin/ruby
+
+# 删除文件 test2.txt
+File.delete("text2.txt")
+
+#文件模式与所有权
+#使用带有掩码的 chmod 方法来改变文件的模式或权限/访问列表：
+#下面的实例改变一个已存在文件 test.txt 的模式为一个掩码值：
+
+#!/usr/bin/ruby
+
+file = File.new( "test.txt", "w" )
+file.chmod( 0755 )
+
+#<<<<<<<<<<<<<<<<<文件查询>>>>>>>>>>>>>>>>>>>>>  File 类用于处理文件
+#<<<<<<<<<<<<<<<<<Ruby 中的目录>>>>>>>>>>>>>>>>  Dir 类用于处理目录
+
+#创建文件 & 临时目录
+#临时文件是那些在程序执行过程中被简单地创建，但不会永久性存储的信息。
+#Dir.tmpdir 提供了当前系统上临时目录的路径，但是该方法默认情况下是不可用的。为了让 Dir.tmpdir 可用，使用必需的 'tmpdir' 是必要的。
+#您可以把 Dir.tmpdir 和 File.join 一起使用，来创建一个独立于平台的临时文件：
+require 'tmpdir'
+   tempfilename = File.join(Dir.tmpdir, "tingtong")
+   tempfile = File.new(tempfilename, "w")
+   tempfile.puts "This is a temporary file"
+   tempfile.close
+   File.delete(tempfilename)
+#这段代码创建了一个临时文件，并向其中写入数据，然后删除文件。Ruby 的标准库也包含了一个名为 Tempfile 的库，该库可用于创建临时文件：
+require 'tempfile'
+   f = Tempfile.new('tingtong')
+   f.puts "Hello"
+   puts f.path
+   f.close
